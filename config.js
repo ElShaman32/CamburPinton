4da203cd441f1efd275a9e49c1ad6bbf65707cf2
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-05-10 18:57:31 -0400

    config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-const TIENDA = {
-    nombre: "SiReBa",
-    rif: "291092270890",
-    moneda: "Bs.",
-    logo: "logo.png",
-    colorPrimario: "#8B5CF6",
-    colorSecundario: "#6D28D9",
-    webhookUrl: "https://ann-familiar-genius-portland.trycloudflare.com/webhook/siemba-webhook",
-    firebase: {
-        apiKey: "AIzaSyDiqM2oykoa-EkTJoFPQSdejjqRthdWxQY",
-        authDomain: "saas-universal-app.firebaseapp.com",
-        projectId: "saas-universal-app",
-        storageBucket: "saas-universal-app.firebasestorage.app",
-        messagingSenderId: "1058808192755",
-        appId: "1:1058808192755:web:a1ebeb9a3625f42cd56186"
-    }
-};
+
