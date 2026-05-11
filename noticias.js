e06169ccabb7db00d0dd4adc0d3f6743fc354d27
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-05-11 02:29:07 -0400

    Actualizacion automatica de noticias

--- noticias.js
+++ noticias.js
@@ -1,1 +1,8 @@
-
+```json
+{
+  "deportes": "Abanderan a delegación de Venezuela para Juegos Deportivas Bolivarianos. Entrevista a Astrid Inojosa y Luz Marina Vargas del baloncesto U17. Venezuela arrasa en...",
+  "espectaculos": "Globovisión abre sus puertas para el segundo casting presencial de Reinas y Reyes de Venezuela. Nuestra planta televisiva se convierte en el epicentro de la...",
+  "tecnologia": "Falcón se convierte en epicentro de la innovación con Fase Regional de las Olimpiadas de Robótica Creativa. mayo 9, 2026.",
+  "mensaje_motivador": "El Ucabista recopiló pensamientos aleccionadores de 15 compatriotas que han destacado por su espíritu ciudadano, sus logros y su amor por el..."
+}
+```
